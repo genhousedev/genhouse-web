9bbbe0c45b0e617864114c1e69f2d3413dbf8b15
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2025-02-24 23:51:51 +0800

    Added assets, colors, footer links

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -6,3 +6,22 @@ export const navLinks = [
     {title: "solutions suite", route: "/solution"},
     {title: "contact", route: "/contact"}
 ]
+
+export const footerLink = [
+        {
+            title: 'projects',
+            children: ['featured project']
+        },
+        {
+            title: 'community',
+            children: ['join our community', 'Careers']
+        },
+        {
+            title: 'about',
+            children: ['who we are', 'contact us', 'testimonials']
+        },
+        {
+            title: 'log in',
+            children: ['sign up', 'volunteer', 'organizations']
+        }
+    ];
