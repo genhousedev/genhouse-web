eb68882a7e2272515017df3bd89009d99b5eaa8b
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2025-03-11 03:19:13 +0800

    fix footer responsive feature

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,11 +7,12 @@ function Footer() {
     
     return (
         <footer className="bg-gold-light text-dark">
-            <div className="flex justify-between px-[6.9062rem] py-[3.125rem]">
+            <div className="flex flex-col gap-9 items-center lg:justify-between px-[6.9062rem] py-[3.125rem] lg:flex-row lg:gap-0">
                 <Image className='inline-block' src='/assets/images/Logo.svg' alt='genhouse-logo' width={240} height={101} />
                 <div className="inline-grid align-top grid-cols-1 md:grid-cols-4 gap-9">
                     {footerLink?.map((link) => (
-                        <div key={link.title} className="mb-6 md:mb-0">
+                        <div key={link.title} className="md:mb-0 text-center lg:text-start">
+                            {/* mb-6 */}
                             <h2 className="text-2xl font-bold mb-3 capitalize">{link.title}</h2>
                             <ul className='text-lg capitalize'>
                                 {link?.children.map((child, index) => (
