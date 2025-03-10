10076728d1ccc9c5adce0494c5104eeadb4b1162
Karl Joseph Sarabia <124430181+karljosephsarabia@users.noreply.github.com>
2025-03-11 02:31:59 +0800

    Fix vercel error in layout.tsx
    Encountered
    
    Failed to compile.
    ./src/app/(root)/layout.tsx
    Module not found: Can't resolve '@/components/Navbar'
    https://nextjs.org/docs/messages/module-not-found
    > Build failed because of webpack errors
    Error: Command "npm run build" exited with 1

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
+import Navbar from '/src/components/Navbar';
 
 function PageLayout({ children }: { children: React.ReactNode; }) {
     return (
